d7cfc6cc57daac73ba2db7c4b88390edc10d1362
Joe DiSalvo <jdisalvo@projetech.com>
2024-12-10 01:29:25 +0000

    day 9

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿using Advent;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine(Day8.Run("/home/coder/code/advent-of-code/inputs/day-8.txt"));
+Console.WriteLine(Day9.Run("/home/coder/code/advent-of-code/inputs/day-9.txt"));
