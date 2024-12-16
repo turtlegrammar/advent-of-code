ae268bd9788930c07442fbb250354ffed0bbac35
Joe DiSalvo <jdisalvo@projetech.com>
2024-12-16 21:22:33 +0000

    extract dijkstra

--- 16.cs
+++ 16.cs
@@ -30,62 +30,15 @@ public static class Day16
             neighbors.AddDict(n, new Node(n.Coordinate, n.Direction.TurnRight()), 1000);
         }
 
-        var (finalCosts, parents) = ShortestPaths(new Node(startPoint, startDirection), neighbors);
+        var (finalCosts, parents) = Algorithms.ShortestPathsFrom(new Node(startPoint, startDirection), neighbors);
 
         var finishNode = List(
             new Node(endPoint, Direction.Left), new Node(endPoint, Direction.Right),
             new Node(endPoint, Direction.Down), new Node(endPoint, Direction.Up)
         ).MinBy(n => finalCosts[n]);
 
-        var coordinatesAlongShortestPath = NodesAlongShortestPathsToNode(parents, finishNode).Select(n => n.Coordinate).ToHashSet();
+        var coordinatesAlongShortestPath = Algorithms.NodesAlongShortestPathsToNode(parents, finishNode).Select(n => n.Coordinate).ToHashSet();
 
         return (finalCosts[finishNode], coordinatesAlongShortestPath.Count);
-
-        // adapted from https://stackoverflow.com/a/77917268
-        (Dictionary<Node, long> costs, Dictionary<Node, HashSet<Node>> parents) ShortestPaths(
-            Node start,
-            Dictionary<Node, Dictionary<Node, long>> neighbors)
-        {
-            var visited = new HashSet<Node>();
-            var costs = new Dictionary<Node, long>() { };
-            var parents = new Dictionary<Node, HashSet<Node>>();
-            costs = neighbors[start].ToDictionary(x => x.Key, x => x.Value);
-
-            var lowestCostUnvisitedNode = FindLowestCostUnvisitedNode();
-            while (lowestCostUnvisitedNode != null)
-            {
-                var cost = costs[lowestCostUnvisitedNode];
-                var ns = neighbors[lowestCostUnvisitedNode];
-                foreach (var (neighbor, costToNeighbor) in ns)
-                {
-                    var newCost = cost + costToNeighbor;
-                    if (!costs.ContainsKey(neighbor) || newCost <= costs[neighbor]) 
-                    {
-                        costs[neighbor] = newCost;
-                        parents.AddSet(neighbor, lowestCostUnvisitedNode);
-                    }
-                }
-
-                visited.Add(lowestCostUnvisitedNode);
-                lowestCostUnvisitedNode = FindLowestCostUnvisitedNode();
-            }
-
-            return (costs, parents);
-
-            // This significantly degrades the performance
-            // C# doesn't have a priority queue with an update function.
-            Node? FindLowestCostUnvisitedNode()
-            {
-                var cs = costs.Where(kvp => !visited.Contains(kvp.Key)).ToList();
-                if (cs.Count == 0)
-                    return null;
-                return cs.MinBy(n => n.Value).Key;
-            }
-        }
-
-        HashSet<Node> NodesAlongShortestPathsToNode(Dictionary<Node, HashSet<Node>> parents, Node n) =>
-            parents.TryGetValue(n, out var ps)
-            ? List(n).Concat(ps.SelectMany(p => NodesAlongShortestPathsToNode(parents, p))).ToHashSet()
-            : new HashSet<Node> { n };
     }
 }
--- Util.cs
+++ Util.cs
@@ -237,3 +237,52 @@ public static class Parse
     public static long[] LongArray(string file) =>
         new Regex("([0-9]+)").Matches(File.ReadAllText(file)).Select(v => Int64.Parse(v.Value)).ToArray();
 }
+
+public static class Algorithms
+{
+        // adapted from https://stackoverflow.com/a/77917268
+        public static (Dictionary<T, long> costs, Dictionary<T, HashSet<T>> parents) ShortestPathsFrom<T>(
+            T start,
+            Dictionary<T, Dictionary<T, long>> neighbors)
+        {
+            var visited = new HashSet<T>();
+            var costs = new Dictionary<T, long>() { };
+            var parents = new Dictionary<T, HashSet<T>>();
+            var queue = new PriorityQueue<T, long>();
+
+            costs = neighbors[start].ToDictionary(x => x.Key, x => x.Value);
+            foreach (var c in costs)
+                queue.Enqueue(c.Key, c.Value);
+
+            while (queue.TryDequeue(out var lowestCostNode, out var _))
+            {
+                // visisted seems to not be necessary, but does speed it up some
+                if (!visited.Contains(lowestCostNode))
+                {
+                    var cost = costs[lowestCostNode];
+                    var ns = neighbors[lowestCostNode];
+                    foreach (var (neighbor, costToNeighbor) in ns)
+                    {
+                        var newCost = cost + costToNeighbor;
+                        // the = case doesn't update the cost but it does add a new parent
+                        if (!costs.ContainsKey(neighbor) || newCost <= costs[neighbor]) 
+                        {
+                            costs[neighbor] = newCost;
+                            // we never need to update https://github.com/dotnet/runtime/issues/44871#issuecomment-2039292354
+                            queue.Enqueue(neighbor, newCost);
+                            parents.AddSet(neighbor, lowestCostNode);
+                        }
+                    }
+
+                    visited.Add(lowestCostNode);
+                }
+            }
+
+            return (costs, parents);
+        }
+
+        public static HashSet<T> NodesAlongShortestPathsToNode<T>(Dictionary<T, HashSet<T>> parents, T node) =>
+            parents.TryGetValue(node, out var ps)
+            ? List(node).Concat(ps.SelectMany(p => NodesAlongShortestPathsToNode(parents, p))).ToHashSet()
+            : new HashSet<T> { node };
+}
